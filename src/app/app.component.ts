8c31404416a12d5e18c2c8dd5f28c7be7482baaa
Nathaniel C. Hall <nathaniel.hall@gmail.com>
2024-02-21 08:19:38 -0500

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,30 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {HeaderComponent} from './header/header.component'
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import { UserComponent } from './user/user.component';
+import { HomeComponent } from './home/home.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet, 
+    HeaderComponent, 
+    MatSidenavModule, 
+    MatIconModule, 
+    MatButtonModule, 
+    MatToolbarModule, 
+    UserComponent,
+    HomeComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'angular-playground';
+  title = 'Playground App';
+
 }
---
+++ src/app/header/header.component.ts
@@ -0,0 +1,17 @@
+import { Component, Input} from '@angular/core';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import {MatToolbarModule} from '@angular/material/toolbar';
+
+@Component({
+  selector: 'app-header',
+  standalone: true,
+  imports: [MatIconModule, MatButtonModule, MatToolbarModule],
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.scss'
+})
+export class HeaderComponent {
+  @Input('click') click: () => void = () => {}
+  @Input('title') title?: string = "Playground"
+
+}
